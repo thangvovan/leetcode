0cd14ce76a66909ef5748b57c6b658c3d69fe51c
Max2Min <haivo5229@gmail.com>
2026-08-03 03:17:24 +0700

    rotate array

---
+++ project/algorithms/RotateArray.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+        for _ in range(k):
+            last = nums.pop()
+            nums.insert(0, last)
