d92a8f5e74fc89b9fab0b9f9c4c302d5b36813c6
Max2Min <haivo5229@gmail.com>
2026-08-04 23:36:09 +0700

    binary tree right side view

---
+++ project/algorithms/BinaryTreeRightSideView.py
@@ -0,0 +1,6 @@
+from typing import List, Optional
+from dataStructures.BinaryTree import TreeNode
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        pass
