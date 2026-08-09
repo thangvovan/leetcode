5dc12f48f25e27abbc2df03651707e0411bafe2a
Max2Min <haivo5229@gmail.com>
2026-08-09 11:15:18 +0700

    house robber 2

--- project/algorithms/HouseRobber.py
+++ project/algorithms/HouseRobber.py
@@ -1,5 +1,32 @@
-from typing import List
+from typing import List, Optional
+from dataStructures.BinaryTree import TreeNode
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        
+        if n == 1:
+            return nums[0]
+        
+        dp = [0] * n
+        
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        
+        return dp[-1] 
+
+    def rob2(self, nums: List[int]) -> int:
+        def dynamic(nums):
+            prev_rob = max_rob = 0
+
+            for cur_val in nums:
+                temp = max(max_rob, prev_rob + cur_val)
+                prev_rob = max_rob
+                max_rob = temp
+            
+            return max_rob
+        
+        return max(dynamic(nums[:-1]), dynamic(nums[1:]), nums[0])
