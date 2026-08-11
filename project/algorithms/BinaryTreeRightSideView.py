c9399b5641cbccd3a1600cc1ef800691a9377aa8
Max2Min <haivo5229@gmail.com>
2026-08-11 19:23:49 +0700

    binary tree right side view

--- project/algorithms/BinaryTreeRightSideView.py
+++ project/algorithms/BinaryTreeRightSideView.py
@@ -3,4 +3,18 @@ from dataStructures.BinaryTree import TreeNode
 
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        pass
+        if not root:
+            return []
+        res, q = [], deque([root])
+
+        while q:
+            size = len(q)
+            for i in range(size):
+                node = q.popleft()
+                if i == size - 1:
+                    res.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return res
