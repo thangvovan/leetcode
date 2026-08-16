03ed162ef61b4a04a1d7b735b794c512cf99fc3d
Max2Min <haivo5229@gmail.com>
2026-08-16 23:05:29 +0700

    course schedule 2

---
+++ project/algorithms/CourseSchedule.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pass
+
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        pass
+
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        pass    
