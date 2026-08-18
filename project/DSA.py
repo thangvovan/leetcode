693323e1621e32d008381fe3ede2e76783c3b080
Max2Min <haivo5229@gmail.com>
2026-08-18 19:19:58 +0700

    implement trie prefix tree

--- project/DSA.py
+++ project/DSA.py
@@ -1,4 +1,11 @@
-from algorithms.PrimeNumberOfSetBitsInBinaryRepresentation import *
+from algorithms.ImplementTriePrefixTree import *
 from dataStructures import *
 
-print(Solution().countPrimeSetBits(10, 15))
+# print(Solution().countPrimeSetBits(10, 15))
+obj = Trie()
+obj.insert("apple")
+print(obj.search("apple"))
+print(obj.search("app"))
+print(obj.startsWith("app"))
+obj.insert("app")
+print(obj.search("app"))
--- project/algorithms/ImplementTriePrefixTree.py
+++ project/algorithms/ImplementTriePrefixTree.py
@@ -1,12 +1,34 @@
+class Node:
+    def __init__(self, val = ""):
+        self.val = val
+        self.children = {}
+
+    def insert(self, child):
+        if child not in self.children:
+            self.children[child] = Node(child)
+        return self.children[child]
+
+    def search(self, word):
+        if len(word) == 0:
+            return self.children
+        w = word[0]
+        if w in self.children:
+            return self.children[w].search(word[1:])
+        return None
+
 class Trie:
     def __init__(self):
-        
+        self.root = Node()
 
     def insert(self, word: str) -> None:
-        
+        node = self.root
+        for w in word:
+            node = node.insert(w)
+        node.insert("\0")
 
     def search(self, word: str) -> bool:
-        
+        node = self.root.search(word)
+        return False if node == None else False if "\0" not in node else True
 
     def startsWith(self, prefix: str) -> bool:
-        
+        return self.root.search(prefix) != None
