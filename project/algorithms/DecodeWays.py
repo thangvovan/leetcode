fd4fd8282d068c453f7ecbe94265a060fd420e27
Max2Min <haivo5229@gmail.com>
2026-08-22 01:55:47 +0700

    decode ways 2

--- project/algorithms/DecodeWays.py
+++ project/algorithms/DecodeWays.py
@@ -22,3 +22,42 @@ class Solution:
             return num
         
         return dynamic(s)
+
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+
+        def dynamic(i: int) -> int:
+            if i in memo:
+                return memo[i]
+            if i == len(s):
+                return 1
+            if s[i] == '0':
+                return 0
+            
+            ans = 0
+            if s[i] == '*':
+                ans += dynamic(i+1) * 9
+            else:
+                ans += dynamic(i+1)
+
+            if i+1 < len(s):
+                if s[i] == '*' and s[i+1] == '*':
+                    ans += dynamic(i+2) * 15
+                elif s[i] != '*' and s[i+1] != '*':
+                    if 10 <= int(s[i] + s[i+1]) <= 26:
+                        ans += dynamic(i+2)
+                elif s[i] != '*' and s[i+1] == '*':
+                    if s[i] == '1':
+                        ans += dynamic(i+2) * 9
+                    elif s[i] == '2':
+                        ans += dynamic(i+2) * 6
+                elif s[i] == '*' and s[i+1] != '*':
+                    if s[i+1] in '0123456':
+                        ans += dynamic(i+2) * 2
+                    elif s[i+1] in '789':
+                        ans += dynamic(i+2)
+
+            memo[i] = ans % (10**9 + 7)
+            return memo[i]
+
+        return dynamic(0)
