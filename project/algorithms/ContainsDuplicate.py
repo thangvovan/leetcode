f47094f022d661ec876ce187eadd36637406fcb9
Max2Min <haivo5229@gmail.com>
2026-08-25 22:02:44 +0700

    contains duplicate 3

--- project/algorithms/ContainsDuplicate.py
+++ project/algorithms/ContainsDuplicate.py
@@ -12,3 +12,19 @@ class Solution(object):
                 return True
             seen[num] = i
         return False
+
+    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        if valueDiff < 0:
+            return False
+        
+        seen = {}
+        for i, x in enumerate(nums): 
+            n = x // (valueDiff+1)
+            if n in seen and i - seen[n][0] <= indexDiff:
+                return True 
+            if n-1 in seen and i - seen[n-1][0] <= indexDiff and abs(x - seen[n-1][1]) <= valueDiff:
+                return True 
+            if n+1 in seen and i - seen[n+1][0] <= indexDiff and abs(x - seen[n+1][1]) <= valueDiff:
+                return True 
+            seen[n] = (i, x) 
+        return False 
