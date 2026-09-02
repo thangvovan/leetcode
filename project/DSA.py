031767c1d10bcc5ff774fb7e196b92d47c019efe
Max2Min <haivo5229@gmail.com>
2026-09-02 17:36:15 +0700

    count complete tree nodes

--- project/DSA.py
+++ project/DSA.py
@@ -1,4 +1,4 @@
-from algorithms.RectangleArea import *
+from algorithms.CountCompleteTreeNodes import *
 from dataStructures import *
 
-print(Solution().computeArea(-3, -3, 3, 3, 4, -5, 5, 0))
+print(Solution().countNodes(binaryTree([1,2,3,4,5,6,7,8,9,10,11])))
--- project/algorithms/CountCompleteTreeNodes.py
+++ project/algorithms/CountCompleteTreeNodes.py
@@ -4,4 +4,43 @@ from dataStructures.BinaryTree import TreeNode
 
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        
+        if root == None:
+            return 0
+        elif root.left == None and root.right == None:
+            return 1
+
+        height = 0
+        node = root
+        while node.left != None:
+            node = node.left
+            height+=1
+
+        def count(root, left, right, h):
+            if root.left == None and root.right == None:
+                return 2
+            elif root.left == None or root.right == None:
+                return 1
+
+            if left == 0:
+                node = root
+                while node.left != None:
+                    node = node.left
+                    left+=1
+            if right == 0:
+                node = root
+                while node.right != None:
+                    node = node.right
+                    right+=1
+
+            if left == right:
+                if left != h:
+                    return (2**(left+1))
+                return 0
+            else:
+                c = count(root.left, left-1, 0, h-1)
+                if c == 0:
+                    return count(root.right, 0, right-1, h-1)
+                else:
+                    return c + 2**right
+
+        return (2**(height+1)-1) - count(root, height, 0, height)
