1036af0a142570e1e8a668d38b55ad39dfe93b0a
Max2Min <haivo5229@gmail.com>
2026-09-23 23:32:02 +0700

    lowest common ancestor of a binary search tree

---
+++ project/algorithms/LowestCommonAncestorOfABinarySearchTree.py
@@ -0,0 +1,5 @@
+from project.dataStructures.BinaryTree import TreeNode
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        pass
